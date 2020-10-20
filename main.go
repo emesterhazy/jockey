6adfd8db36aba279e06474fcdd7ea6148cf58aa9
Evan Mesterhazy <evan.mesterhazy@gmail.com>
2020-10-20 19:20:06 -0400

    Clean up methods for requesting and dumping HTTP requests

--- main.go
+++ main.go
@@ -27,54 +27,23 @@ func main() {
 		"(Required) The URL to send requests. Defaults to https unless http is specified")
 	flag.Parse()
 
+	// Parse user's URL
 	if *targetURL == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
-
 	parsed, err := fuzzyParseURL(*targetURL)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	port := 80
 	if parsed.Port() != "" {
 		port, _ = strconv.Atoi(parsed.Port())
 	}
-	conn, err := request(parsed.Hostname(), parsed.Path, port, nil)
+
+	err = dumpHTTP(io.Writer(os.Stdout), parsed.Hostname(), parsed.Path, port, nil)
 	if err != nil {
 		log.Fatal(err)
-	}
-	reader := bufio.NewReader(conn)
-	tp := textproto.NewReader(reader)
-
-	for {
-		line, err := tp.ReadLine()
-		if err != nil {
-			break
-		}
-		//fmt.Println(line)
-		if line == "" {
-			break
-		}
-	}
-	buf := make([]byte, 4096*16)
-	for {
-		n, err := reader.Read(buf)
-		if err != nil && err != io.EOF {
-			conn.Close()
-			log.Fatal(err)
-		} else if err == io.EOF {
-			err := conn.Close()
-			os.Stdout.Write(buf[:n])
-			if err != nil {
-				log.Print("error closing connection")
-				log.Print(err)
-			}
-			//fmt.Println("end of request response")
-			break
-		}
-		os.Stdout.Write(buf[:n])
 	}
 
 	os.Exit(0)
@@ -104,7 +73,40 @@ func fuzzyParseURL(rawURL string) (*url.URL, error) {
 	return nil, fmt.Errorf("invalid URL: %s\n", originalURL)
 }
 
-func request(host string, path string, port int, headers *map[string]string) (*net.TCPConn, error) {
+// Make an HTTP request and dump the response to writer
+func dumpHTTP(writer io.Writer, host string, path string, port int,
+	headers *map[string]string) error {
+
+	conn, err := openRequest(host, path, port, headers)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	// New buffered reader with 16 page buffer
+	reader := bufio.NewReaderSize(conn, 0x1000*16)
+	tp := textproto.NewReader(reader)
+	// Skip headers
+	for {
+		line, err := tp.ReadLine()
+		if err != nil {
+			return err
+		}
+		if line == "" {
+			break
+		}
+	}
+
+	_, err = reader.WriteTo(writer)
+	if err != nil && err != io.EOF {
+		return err
+	}
+	return nil
+
+}
+
+// Send an HTTP request and return an open TCP connection
+func openRequest(host string, path string, port int, headers *map[string]string) (*net.TCPConn, error) {
 	// Based on the way I'm interpreting the assignment I'm specifically avoiding
 	// the net.Dial function and manually looking up the host address
 
@@ -127,20 +129,27 @@ func request(host string, path string, port int, headers *map[string]string) (*n
 	addrs, _ := net.LookupHost(host)
 	for _, addr := range addrs {
 		tcpAddr := &net.TCPAddr{IP: net.ParseIP(addr), Port: port}
+		// TODO: Add a timeout here
 		conn, err := net.DialTCP("tcp", nil, tcpAddr)
 		if err != nil {
 			log.Print(err)
 			continue
 		}
-		//reqLine := fmt.Sprintf("GET %s HTTP/1.1\r\n\r\n", path)
-		//b := []byte(reqLine)
-		fmt.Fprintf(conn, "GET %s HTTP/1.1\r\n", path)
+		// Write HTTP request line and headers
+		writer := bufio.NewWriter(conn)
+		// Buffered writer will noop after first error so we only check on final Flush()
+		_, _ = fmt.Fprintf(writer, "GET %s HTTP/1.1\r\n", path)
 		for header, value := range reqHeaders {
-			fmt.Fprintf(conn, "%s: %s\r\n", header, value)
+			_, _ = fmt.Fprintf(writer, "%s: %s\r\n", header, value)
 		}
-		fmt.Fprint(conn, "\r\n")
-
+		_, _ = fmt.Fprint(writer, "\r\n")
+		err = writer.Flush()
+		if err != nil {
+			conn.Close()
+			return nil, err
+		}
 		return conn, nil
 	}
+	// Exhausted all host address options without connecting
 	return nil, errors.New("could not connect to host")
 }
